7274cf2668abf952ed45b0683c5c19894eb117cb
Adam <Adam.ja.com@op.pl>
2022-12-19 21:32:44 +0100

    plant works at all

--- src/main/java/com/akgroup/project/world/object/Plant.java
+++ src/main/java/com/akgroup/project/world/object/Plant.java
@@ -2,10 +2,16 @@ package com.akgroup.project.world.object;
 
 import com.akgroup.project.util.Vector2D;
 
-public class Plant extends AbstractWorldElement{
+public class Plant extends AbstractWorldElement {
+    private int nutritionalValue;
 
     public Plant(Vector2D position) {
         super(position);
+        this.nutritionalValue = 3;
+    }
+
+    public int getNutritionalValue() {
+        return nutritionalValue;
     }
 
     @Override
--- src/test/java/com/akgroup/project/world/object/PlantTest.java
+++ src/test/java/com/akgroup/project/world/object/PlantTest.java
@@ -1,4 +1,30 @@
+package com.akgroup.project.world.object;
+
+import com.akgroup.project.util.Vector2D;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlantTest {
-  
+    private Plant plant1;
+    private IWorldElement plant2;
+
+    @BeforeEach
+    void setPlant1() {
+        plant1 = new Plant(new Vector2D(1, 2));
+        plant2 = new Plant(new Vector2D(5, -1));
+    }
+
+    @Test
+    void getNutritionalValue() {
+        assertEquals(plant1.getNutritionalValue(), 3);
+        assertEquals(((Plant) plant2).getNutritionalValue(), 3);
+    }
+
+    @Test
+    void getType() {
+        assertEquals(plant1.getType(), TypeEnum.PLANT);
+        assertEquals(plant2.getType(), TypeEnum.PLANT);
+    }
 }
